92cdefa93b7ac7734bf6ed2f8ff57d2e0755a0bb
APAC\shdeshmu <shyamsundar.deshmukh.consultant@nielsen.com>
2018-01-08 14:10:04 -0500

    first commit

---
+++ src/main/java/ca/uwo/csd/cs2212/USERNAME/App.java
@@ -0,0 +1,13 @@
+package ca.uwo.csd.cs2212.USERNAME;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/ca/uwo/csd/cs2212/USERNAME/TestBankAccount.java
@@ -0,0 +1,5 @@
+package ca.uwo.csd.cs2212.USERNAME;
+
+public class TestBankAccount {
+
+}
